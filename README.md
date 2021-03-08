185277ea945c7f3c617e2ed54c5aa31e69594854
arai1999 <arai1999@hotmail.com>
2021-03-08 00:16:13 -0800

    Added bubbleSort

--- README.md
+++ README.md
@@ -2,8 +2,6 @@
 Here is a program that will visualize different sorting algorithms, just for practice.
 
 To DO:
-Selection Sort Algorithm
-Bubble Sort Algorithm
 Merge Sort Algorithm
 Iterative Merge Sort Algorithm
 Quicksort Algorithm
---
+++ bubbleSort.java
@@ -0,0 +1,37 @@
+package SortingVisualizer;
+
+import java.util.Arrays;
+
+public class bubbleSort {
+    public static void main(String[] args) {
+        int arr[] = {3,8,5,4,1,9,-2};
+        //Print OG
+        System.out.println("Original array");
+        System.out.println(Arrays.toString(arr) + "\n");
+        
+        bubbleSorter(arr);
+        //Print Sorted
+        System.out.println("Sorted array");
+        System.out.println(Arrays.toString(arr));
+    }
+    //Function to perform bubble sort
+    public static void bubbleSorter(int[] arr) {
+        //n-1 passes n is array's length
+        for(int k = 0; k < arr.length - 1; k++) {
+            //last k is already sorted so inner loop
+            //can avoid k
+            for(int i = 0; i < arr.length - 1 - k; i++) {
+                if(arr[i] > arr[i + 1]) {
+                    swap(arr, i, i + 1);
+                }
+            }
+            //Algorithm terminates if inner loop does no swaps
+        }
+    }
+    //Function to swap values
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
