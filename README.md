d5e1b206376bfd1fe523ea3f9f063f92d4492fb1
arai1999 <arai1999@hotmail.com>
2021-03-08 00:03:05 -0800

    Added selection sort

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Sorting-Algortithm-Visualizer
 Here is a program that will visualize different sorting algorithms, just for practice.
+
+To DO:
+Selection Sort Algorithm
+Bubble Sort Algorithm
+Merge Sort Algorithm
+Iterative Merge Sort Algorithm
+Quicksort Algorithm
+Heap Sort Algorithm
---
+++ selectionSort.java
@@ -0,0 +1,38 @@
+package SortingVisualizer;
+
+import java.util.Arrays;
+
+public class selectionSort {
+    public static void main(String[] args) {
+        int arr[] = {3,8,5,4,1,9,-2};
+        //Print OG
+        System.out.println("Original array");
+        System.out.println(Arrays.toString(arr) + "\n");
+        
+        selectionSorter(arr);
+        //Print Sorted
+        System.out.println("Sorted array");
+        System.out.println(Arrays.toString(arr));
+    }
+    //Function to perform selection sort
+    public static void selectionSorter(int[] arr) {
+        //run n-1 times where n is array length
+        for(int i = 0; i < arr.length - 1; i++) {
+            //find the minimum element in unsorted list
+            //swap it with arr[i]
+            int min = i;
+            for(int j = i + 1; j < arr.length; j++) {
+                if(arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+            swap(arr,min,i);
+        }
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
